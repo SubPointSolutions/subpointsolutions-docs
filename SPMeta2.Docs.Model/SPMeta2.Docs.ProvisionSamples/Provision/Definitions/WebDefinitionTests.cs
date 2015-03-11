bab47cd259a6a615606383d0b046968a72054b2e
SubPointSupport <support@subpointsolutions.com>
2015-03-10 23:04:41 -0700

    WebDefinition provision sketches.

---
+++ SPMeta2.Docs.Model/SPMeta2.Docs.ProvisionSamples/Provision/Definitions/WebDefinitionTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Docs.ProvisionSamples.Base;
+using SPMeta2.Docs.ProvisionSamples.Definitions;
+using SPMeta2.Syntax.Default;
+
+namespace SPMeta2.Docs.ProvisionSamples.Samples
+{
+    [TestClass]
+    public class WebDefinitionTest : ProvisionTestBase
+    {
+        #region methods
+
+        [TestMethod]
+        [TestCategory("Docs.WebDefinition")]
+        public void CanDeploySimpleWebs()
+        {
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddWeb(DocWebs.News);
+                web.AddWeb(DocWebs.AboutOurCompany);
+            });
+
+            DeployModel(model);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Docs.WebDefinition")]
+        public void CanDeployHierarchicalWebs()
+        {
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web
+                    .AddWeb(DocWebs.News)
+                    .AddWeb(DocWebs.Departments, departmentWeb =>
+                    {
+                        departmentWeb
+                            .AddWeb(DocWebs.DepartmentWebs.HR)
+                            .AddWeb(DocWebs.DepartmentWebs.ITHelpDesk, itWeb =>
+                            {
+                                itWeb
+                                    .AddWeb(DocWebs.DepartmentWebs.ITHelpDeskWebs.Apple)
+                                    .AddWeb(DocWebs.DepartmentWebs.ITHelpDeskWebs.Cisco)
+                                    .AddWeb(DocWebs.DepartmentWebs.ITHelpDeskWebs.Microsoft);
+                            })
+                            .AddWeb(DocWebs.DepartmentWebs.Sales);
+                    })
+                    .AddWeb(DocWebs.AboutOurCompany);
+            });
+
+            DeployModel(model);
+        }
+
+        #endregion
+    }
+}
