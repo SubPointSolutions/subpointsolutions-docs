61a19aaa8865a0da73ff84bb2b4986dc23ffe58f
Support <support@subpointsolutions.com>
2016-02-21 23:35:50 +1100

    + Wyam PS script port fix, ready for the docs migration

--- SubPointSolutions.Docs/_start-site.ps1
+++ SubPointSolutions.Docs/_start-site.ps1
@@ -4,5 +4,5 @@ $wyam = "Wyam 0.11.1-beta"
 
 cd  $PSScriptRoot
 
-Start-Process "chrome.exe" http://localhost:50800
+Start-Process "chrome.exe" http://localhost:5080
 . "$PSScriptRoot\$wyam\Wyam.exe"  --input "$PSScriptRoot\Views" --output "$PSScriptRoot\Views-Output" --preview --watch --config "_site.wyam" 
