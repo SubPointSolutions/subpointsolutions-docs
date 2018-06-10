8c75588cdc7033fb5fcb5c611a7d1e9a46629d8a
subpointsupport <support@subpointsolutions.com>
2018-06-10 21:35:36 +1000

    + arf fix 2

--- src/docs-build.rb
+++ src/docs-build.rb
@@ -33,7 +33,7 @@ parser = OptionParser.new do|opts|
       options[:site_cache_path] = name;
     end
 
-    opts.on('-p', '--parallel=VALUE', 'Enable parallel build') do |name|
+    opts.on('-p', '--parallel', 'Enable parallel build') do |name|
       options[:enable_parallel_build] = true;
     end
     
